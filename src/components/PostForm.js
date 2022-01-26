9f57d2890dfc8d50b9c7c94389da23ef54e9706b
alexsincai <alex.sincai@yahoo.co.uk>
2022-01-26 13:56:09 +0200

    updated form a bit

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -2,9 +2,10 @@ import * as React from "react";
 import { useState } from "react";
 import { useAtom } from "jotai";
 import state from "../state";
+import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
-import Box from "@mui/material/Box";
+import UploadIcon from "@mui/icons-material/Upload";
 
 const PostForm = () => {
     const [text, setText] = useState("");
@@ -16,8 +17,6 @@ const PostForm = () => {
 
         const formData = new FormData(e.target);
         formData.append("userid", user.id);
-        console.log(e.target);
-        console.log(formData);
 
         const req = await fetch(process.env.REACT_APP_SERVER_URL, {
             method: "POST",
@@ -42,11 +41,9 @@ const PostForm = () => {
 
     return (
         <Box
-            // component="form"
             sx={{ "& .MuiTextField-root": { m: 1, width: "62ch" } }}
             noValidate
             autoComplete="off"
-            // onSubmit={handlePost}
         >
             <form
                 onSubmit={handlePost}
@@ -63,22 +60,22 @@ const PostForm = () => {
                     defaultValue={text}
                     onChange={updateText}
                     form="starForm"
-                />
-                <p className="form-text">
-                    {200 - text.length} characters remaining
-                </p>
-                <input
-                    accept="image/*"
-                    // className={classes.input}
-                    style={{ display: "none" }}
-                    id="raised-button-file"
-                    // multiple
-                    className="form-control"
-                    type="file"
-                    form="starForm"
+                    helperText={`${200 - text.length} characters remaining`}
                 />
                 <label htmlFor="raised-button-file">
-                    <Button variant="raised" component="span">
+                    <input
+                        type="file"
+                        accept="image/*"
+                        id="raised-button-file"
+                        name="file"
+                        style={{ display: "none" }}
+                        form="starForm"
+                    />
+                    <Button
+                        variant="raised"
+                        component="span"
+                        startIcon={<UploadIcon />}
+                    >
                         Upload image
                     </Button>
                 </label>
